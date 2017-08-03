7e08e19d4bca1214df3916ee2bfb43b0508ea8a5
poonam patel <patel.poonam4@gmail.com>
2017-08-03 17:31:02 +0530

    connect disconnect server client messages added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname+'/../public')
-var app = express();
- app.use(express.static(publicPath));
  const _port = process.env.PORT||3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+ io.on('connection',(socket)=>{
+     console.log('New User Connected');
+
+     socket.on('disconnect',()=>{
+        console.log('Disconnected from server host'); 
+     });
+ });
+ app.use(express.static(publicPath));
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(_port,()=>
-{
+server.listen(_port,()=>{
 console.log(`server is listening at port ${_port}`);
 });
